8a2e7c4d8ed6aa4d78a9030075984b0f8a4c58d7
matteo110918 <matteo_palmieri@hotmail.com>
2023-09-01 08:53:58 +0200

    Fertigstellung Programm

--- mapIt.py
+++ mapIt.py
@@ -3,3 +3,12 @@
 # command line or clipboard
 
 import webbrowser, sys, pyperclip
+
+if (len(sys.argv) > 1):
+    # Get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open(f'https://www.google.com/maps/place/{address}')
